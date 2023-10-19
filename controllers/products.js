573975b66d5c35d081630870f639bd436b1e44f7
X-RAY <intelekt200012@gmail.com>
2023-10-19 20:21:38 +0300

    add products endpoints

---
+++ controllers/products.js
@@ -0,0 +1,42 @@
+const Products = require('../models/products');
+const ProductsCategories = require('../models/productsCategories');
+const { controllerWrapper } = require('../utils');
+
+/**
+ * Get all products from database.
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+
+ * @returns {Object} JSON response containing the products.
+ */
+
+const getAllProducts = controllerWrapper(async (req, res) => {
+  // await for products array from db
+  const result = await Products.find();
+
+  //  Response with the object of products
+  res.status(200).json(result);
+});
+
+/**
+ * Get products categories from database.
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+
+ * @returns {Object} JSON response containing the products categories
+ */
+
+const getProductsCategories = controllerWrapper(async (req, res) => {
+  // await for products categories array from db
+  const result = await ProductsCategories.find();
+
+  //  Response with the object  of products categories
+  res.status(200).json(result);
+});
+
+module.exports = {
+  getAllProducts,
+  getProductsCategories,
+};
