af98706e80477ac189c11d0245d3b7b228ebc490
SirotovAlexander <alexander.sirotov@gmail.com>
2023-10-20 16:14:39 +0300

    add validate middle
    add validate middle

---
+++ utils/validation/diaryValidationSchemas.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+
+const { validateErrorMessageList } = require('../../variables');
+
+/**
+ * Joi schema for validating the request body when product add in diary.
+ */
+const productSchema = Joi.object({
+  product_ID: Joi.string().required(),
+  date: Joi.string().format('dd/mm/YYYY').required(),
+  amount: Joi.number().required(),
+  calories: Joi.number().required(),
+}).messages(validateErrorMessageList);
+
+/**
+ * Joi schema for validating the request body when exercise add in diary.
+ */
+const exerciseSchema = Joi.object({
+  exercise_ID: Joi.string().required(),
+  date: Joi.string().format('dd/mm/YYYY').required(),
+  time: Joi.number().required(),
+  calories: Joi.number().required(),
+}).messages(validateErrorMessageList);
+
+module.exports = {
+  productSchema,
+  exerciseSchema,
+};
