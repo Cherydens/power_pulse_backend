a43b023e067eb2c0d3d2060242d6323cdf3f203f
SirotovAlexander <alexander.sirotov@gmail.com>
2023-10-20 12:32:11 +0300

    Update diary.js

--- controllers/diary.js
+++ controllers/diary.js
@@ -2,6 +2,7 @@ const Products = require('../models/products');
 const Exercises = require('../models/exercises');
 const { controllerWrapper } = require('../utils/wrappers/controllerWrapper');
 
+// Контролер отримання всіх вправ та продуктів що містяться в щоденнику користувача
 const getDashboard = controllerWrapper(async (req, res) => {
   const productResult = await Products.find();
   const exercisestResult = await Exercises.find();
@@ -10,6 +11,7 @@ const getDashboard = controllerWrapper(async (req, res) => {
   res.status(200).json(result);
 });
 
+// Контролер отримання вправ та продуктів що містяться в щоденнику користувача за визначену дату
 const getDayDashboard = controllerWrapper(async (req, res) => {
   const { date } = req.query;
   const productResult = await Products.find({ dateOfuse: date });
