4d1d403cc4fa9838d234e3b1458917dbff2a2255
X-RAY <intelekt200012@gmail.com>
2023-10-21 13:16:01 +0300

    add pagination on routes

--- controllers/training/getAllExercises.js
+++ controllers/training/getAllExercises.js
@@ -2,7 +2,24 @@ const Exercises = require('../../models/exercises');
 const { controllerWrapper } = require('../../utils');
 
 const getAllExercises = controllerWrapper(async (req, res) => {
-  const result = await Exercises.find();
+  const {
+    bodyPart = '',
+    equipment = '',
+    target = '',
+    page = 1,
+    limit = 18,
+  } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Exercises.find(
+    {
+      bodyPart: { $regex: bodyPart, $options: 'i' },
+      equipment: { $regex: equipment, $options: 'i' },
+      target: { $regex: target, $options: 'i' },
+    },
+    {},
+    { skip, limit }
+  );
 
   res.status(200).json(result);
 });
