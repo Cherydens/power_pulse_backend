822001a3ab0346e1923792c4d787d9d227cf582a
SirotovAlexander <alexander.sirotov@gmail.com>
2023-10-22 21:51:00 +0300

    fix
    fix

--- controllers/diary/deleteExercise.js
+++ controllers/diary/deleteExercise.js
@@ -4,7 +4,7 @@ const { controllerWrapper } = require('../../utils/index');
 // Контролер видалення вправи що містяться в щоденнику користувача за визначену дату
 const deleteExercise = controllerWrapper(async (req, res) => {
   const { exerciseId } = req.params;
-  console.log('BEBEBEBBE');
+
   const result = await Exercises.findByIdAndRemove(exerciseId);
   //  Response with the object of exercises
   res.status(202).json(result);
