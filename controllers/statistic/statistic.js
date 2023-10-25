41ca5eddfa530303ed367efe9c4575150d071e08
X-RAY <intelekt200012@gmail.com>
2023-10-25 13:21:47 +0300

    Update statistic.js

--- controllers/statistic/statistic.js
+++ controllers/statistic/statistic.js
@@ -1,11 +1,37 @@
 const Exercises = require('../../models/exercises');
 const User = require('../../models/user');
 const DiaryExercises = require('../../models/exercisesInDiary');
-const { controllerWrapper, HttpError } = require('../../utils');
+
+const { controllerWrapper } = require('../../utils');
 
 const getStatistic = controllerWrapper(async (req, res) => {
-  const exerciseTotalCounter = await Exercises.find();
-  console.log(exerciseTotalCounter.length - 1);
+  const allExercises = await Exercises.find();
+  const allDiaryExercises = await DiaryExercises.find();
+  const allUsers = await User.find();
+
+  const exerciseTotalQuantity = allExercises.length;
+
+  const caloriesTotalQuantity = allDiaryExercises.reduce((acc, exercise) => {
+    return acc + exercise.calories;
+  }, 0);
+
+  const timeTotalQuantity = allDiaryExercises.reduce((acc, exercise) => {
+    return acc + exercise.time;
+  }, 0);
+
+  const usersTotalQuantity = allUsers.length;
+
+  const exerciseDoneTotalQuantity = allDiaryExercises.length;
+
+  const result = {
+    exerciseTotalQuantity,
+    caloriesTotalQuantity,
+    usersTotalQuantity,
+    timeTotalQuantity,
+    exerciseDoneTotalQuantity,
+  };
+
+  res.status(200).json(result);
 });
 
 module.exports = {
