7e4272e736f8225d790112d52b2386bf8b5a1332
Denys Cherednychenko <cherydens@ukr.net>
2023-11-01 15:50:11 +0200

    Update birthday in userParams

--- controllers/users/getUserParams.js
+++ controllers/users/getUserParams.js
@@ -1,11 +1,35 @@
+const { format } = require('date-fns');
 const { controllerWrapper, getBmr } = require('../../utils');
 
 const getUserParams = controllerWrapper(async (req, res) => {
   const { userParams } = req.user;
+  let updatedUserParams = null;
+
+  if (userParams) {
+    const {
+      height,
+      currentWeight,
+      desiredWeight,
+      birthday,
+      blood,
+      sex,
+      levelActivity,
+    } = userParams;
+
+    updatedUserParams = {
+      height,
+      currentWeight,
+      desiredWeight,
+      birthday: format(new Date(birthday), 'yyyy-MM-dd'),
+      blood,
+      sex,
+      levelActivity,
+    };
+  }
 
   res.status(200).json({
     user: {
-      userParams,
+      userParams: updatedUserParams,
     },
     bmr: getBmr(userParams),
   });
--- controllers/users/updateUserParams.js
+++ controllers/users/updateUserParams.js
@@ -1,9 +1,12 @@
+const { format } = require('date-fns');
 const User = require('../../models/user');
 const { getBmr, controllerWrapper } = require('../../utils');
 
 const updateUserParams = controllerWrapper(async (req, res) => {
   const { _id } = req.user;
   const userParams = { ...req.body };
+
+  userParams.birthday = format(new Date(userParams.birthday), 'yyyy-MM-dd');
 
   await User.findByIdAndUpdate(_id, { userParams });
 
